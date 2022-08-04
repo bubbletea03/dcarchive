0b71eb9edcb77893d659c1f39fad3e36a0f637bf
기윤 <bubbletea03@naver.com>
2022-08-04 20:33:11 +0900

    리드미 추가해봄

---
+++ README.md
@@ -0,0 +1,4 @@
+참고 사이트
+
+https://thrillfighter.tistory.com/413
+`파이썬 tkinter에서 쓰레드를 사용하려면`
--- main.py
+++ main.py
@@ -8,8 +8,8 @@ def main():
 
 class App(Tk):
     def __init__(self) -> Tk:
-        Tk.__init__(self)
-        self.title("DCArchiver")
+        super().__init__()
+        self.title("dcArchiver")
 
         self.listbox = Listbox(self, selectmode="extended", width=50, height=20)
         self.listbox.bind('<<ListboxSelect>>', self.onSelect)
@@ -22,8 +22,7 @@ class App(Tk):
         self.textarea = Text(self.right_frame, width=30, height=10)
         self.textarea.pack(side="top", padx=10, pady=10)
 
-        self.start_btn = Button(self.right_frame, text="기록 시작", padx=10, pady=10)
-        self.start_btn.pack()
+        Button(self.right_frame, text="기록 시작", padx=10, pady=10).pack()
 
     def onSelect(self, event):
         w = event.widget
@@ -35,11 +34,11 @@ class App(Tk):
             self.textarea.delete("1.0", END)
             self.textarea.insert(END, f.readline())
 
-    def fill_listbox_withPosts(self, listbox : Listbox):
+    def fill_listbox_withPosts(self):
         filenames = os.listdir("./posts")
         for filename in reversed(filenames):
             with open("./posts/"+filename, "r", encoding="utf8") as f:
-                listbox.insert(END, filename + " │ " + f.readline())
+                self.listbox.insert(END, filename + " │ " + f.readline())
 
     async def start_archive(self):
         while True:
