74649d5fe9ea03fd3227d74ff5d45c634d538822
기윤 <bubbletea03@naver.com>
2022-08-05 15:10:08 +0900

    스크롤, 축소로 스샷 깔끔하게 찍히게 함, try catch 추가

--- recorder.py
+++ recorder.py
@@ -33,16 +33,21 @@ def save_pageArchive():
             "content": content_div.text if content_div else ""
         }
         
-        with open(os.getcwd()+"/posts/"+data["num"], "w", encoding="utf8") as f:
-            f.write(data["title"])
-            f.write(data["content"])
-        driver.get_screenshot_as_file(os.getcwd()+"/screenshots/"+data["num"]+".png")
+        try:
+            with open(os.getcwd()+"/posts/"+data["num"], "w", encoding="utf8") as f:
+                f.write(data["title"])
+                f.write(data["content"])
+            driver.execute_script("window.scrollTo(135, 230)")
+            driver.execute_script("document.body.style.zoom='80%'")
+            driver.get_screenshot_as_file(os.getcwd()+"/screenshots/"+data["num"]+".png")
+        except:
+            print("saving file failed")
 
 def get_driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("disable-gpu")
-    options.add_argument("window-size=1080x1080")
+    options.add_argument("window-size=640x640")
     options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
 
     driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
